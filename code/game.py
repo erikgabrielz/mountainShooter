5793773e45d4d31b56c6df6e4f3d3e7d38806d51
Erik <farmingsimulatorpc@gmail.com>
2024-11-12 16:56:12 -0300

    include background and music on menu

--- code/game.py
+++ code/game.py
@@ -1,11 +1,17 @@
 import pygame
+from code.Const import WIN_HEIGHT, WIN_WIDTH
 from code.Menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+        #load music
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        #play music
+        pygame.mixer_music.play(-1)
+
         while True:
             menu = Menu(self.window)
             menu.run()
--- code/menu.py
+++ code/menu.py
@@ -1,6 +1,16 @@
+import pygame.image
+
 class Menu:
     def __init__(self, window):
         self.window = window
+        #load image
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        #create rect
+        self.rect = self.surf.get_rect(left = 0, top = 0)
 
     def run(self):
+        #display image on rect
+        self.window.blit(source=self.surf, dest=self.rect)
+        #update display
+        pygame.display.flip()
         pass
