52eb7ae9b3921be6427ab01cd698383756e57f60
Erik <farmingsimulatorpc@gmail.com>
2024-11-25 10:09:58 -0300

    creating mediator pattern
    Enemies Destruction

--- code/Const.py
+++ code/Const.py
@@ -4,6 +4,25 @@ COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (246, 210, 38)
 
+
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1'  : 300,
+    'Player1Shot': 1,
+    'Player2'  : 300,
+    'Player2Shot': 1,
+    'Enemy1'    : 50,
+    'Enemy1Shot':  1,
+    'Enemy2'    : 60,
+    'Enemy2Shot':  1,
+}
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -17,6 +36,7 @@ ENTITY_SPEED = {
     'Enemy1'   : 2,
     'Enemy2'   : 1
 }
+
 EVENT_ENEMY = pygame.USEREVENT + 1
 
 MENU_OPTION = ("NEW GAME 1P",
