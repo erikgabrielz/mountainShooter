340ba8bd899cfaa413351a98c6bd2b9a350b67dd
Erik <farmingsimulatorpc@gmail.com>
2024-11-26 15:11:59 -0300

    Level 2 implementation

--- code/Game.py
+++ code/Game.py
@@ -3,6 +3,7 @@ from code.Const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
 from code.Menu import Menu
 from code.Level import Level
 
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -15,11 +16,16 @@ class Game:
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, 'Level1', menu_return)
-                level_return = level.run()
+                player_score = [0, 0] #[Player1, Player2]
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return, player_score)
+                    level_return = level.run(player_score)
+
             elif menu_return == MENU_OPTION[4]:
-                pygame.quit() #close window
-                quit() #end pygame
+                pygame.quit()  # close window
+                quit()  # end pygame
             else:
                 pass
-
