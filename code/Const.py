17b8746420ff476d053f90ceb9371d2780330a97
Erik <farmingsimulatorpc@gmail.com>
2024-11-26 15:11:59 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -14,6 +14,11 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1'  : 1,
     'Player1Shot': 25,
     'Player2'  : 1,
@@ -32,9 +37,14 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
-    'Player1'  : 300,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
+    'Player1'  : 200,
     'Player1Shot': 1,
-    'Player2'  : 300,
+    'Player2'  : 200,
     'Player2Shot': 1,
     'Enemy1'    : 50,
     'Enemy1Shot':  1,
@@ -50,6 +60,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1'  : 0,
     'Player1Shot': 0,
     'Player2'  : 0,
@@ -68,6 +83,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1'  : 3,
     'Player1Shot': 6,
     'Player2'  : 3,
@@ -86,6 +106,7 @@ ENTITY_SHOT_DELAY = {
 }
 
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 
 MENU_OPTION = ("NEW GAME 1P",
              "NEW GAME 2P - COOPERATIVE",
@@ -105,6 +126,9 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                    'Player2': pygame.K_LCTRL}
 
 SPAWN_TIME = 4000
+
+TIMEOUT_LEVEL = 20000 # 20s
+TIMEOUT_STEP = 100    #100ms
 
 WIN_WIDTH = 476
 WIN_HEIGHT = 324
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -8,13 +8,19 @@ from code.Enemy import Enemy
 class EntityFactory:
 
     @staticmethod
-    def get_entity(entity_name: str, position = (0, 0)):
+    def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range(7):
+                for i in range(7):  # level1bg images number
                     list_bg.append(Background(f'Level1Bg{i}', position))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):  # level2bg images number
+                    list_bg.append(Background(f'Level2Bg{i}', position))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player('Player1', (10, (WIN_HEIGHT / 2) - 30))
